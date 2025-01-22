a8f3590bd12f9e33480ea12fead75ecc67fc6270
df42Karazal <176348199+df42Karazal@users.noreply.github.com>
2025-01-21 23:24:56 -0300

    trigonometry 1

---
+++ theCpart/cos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+#include "something.h"
+
+int main(double argc, double **argv) {
+    // code here
+   int cosof = cos(argc, argv);
+   return EXIT_SUCCESS;
+}
