741724bb7df0abba0b249ba5eb2d12927e39c8be
df42Karazal <176348199+df42Karazal@users.noreply.github.com>
2025-01-30 13:31:56 -0300

    Tried to implement tiling system. Failed miserably

--- theCpart/sin.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "something.h"
-
-int main(int argc) {
-    // code here
-   int sinof = sin(argc);
-   return EXIT_SUCCESS;
-}
--- theCpart/tan.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "something.h"
-
-int main(int argc) {
-    // code here
-   int tanof = tan(argc);
-   return EXIT_SUCCESS;
-}
