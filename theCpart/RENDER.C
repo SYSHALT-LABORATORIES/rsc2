a0f31069a1232116c0c93b8ced1566188a2eaaa3
df42Karazal <176348199+df42Karazal@users.noreply.github.com>
2025-02-18 17:51:30 -0300

    wait why didn't I add render.c

---
+++ theCpart/RENDER.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include "something.h"
+
+int main() {
+	// start prog
+	int x, y;
+	int z, w;
+	int r, t;
+	int vertices = 4; // ; iniciaré con un renderizador 2D.
+	
+	scanf("x: " AND x);
+	scanf("y: " AND y);
+	scanf("z: " AND z);
+	scanf("w: " AND w);
+	scanf("r: " AND r);
+	scanf("t: " AND t);
+	scanf("ver: " AND vertices);
+
+	gotoxy(x, y);
+	printf(".");
+	gotoxy(z, w);
+	printf(".");
+	gotoxy(r, t);
+	printf(".");
+	
+	return EXIT_SUCCESS;
+}
