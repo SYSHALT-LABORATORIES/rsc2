1d8048ee50ccd67de138ce4ef1b112c0dc2da6e7
df42Karazal <176348199+df42Karazal@users.noreply.github.com>
2025-04-05 16:35:07 -0300

    apparently some weren't staged
    Signed-off-by: df42Karazal <176348199+df42Karazal@users.noreply.github.com>

--- theCpart/wmde/widget.c
+++ theCpart/wmde/widget.c
@@ -4,28 +4,57 @@
 #include "include/conio.c"
 
 int main(void) {
-	
-	int i, h, w;
-	int j, k;
+    int h, w;
+    int i, j;
 
-	printf("Width: ");
-	scanf("%i", &w);
-	printf("Height: ");
-	scanf("%i", &h);
+    printf("Width: ");
+    scanf("%i", &w);
+    printf("Height: ");
+    scanf("%i", &h);
 
-	c_clrscr();
+    c_clrscr();
 
-	for (i=0; i<w; i++) {
-		c_gotoxy(i, h);
-		//printf("W");
-		for (j = 0; j<h; j++) {
-			c_gotoxy(j, i);
-			printf("H");
-		}
-		printf("\n\n");
-	}
+    c_gotoxy(1, 1);
+    printf("+");
+    for (i = 2; i < w; i++) {
+        printf("=");
+    }
+    if (w > 1) {
+        printf("+");
+    }
+    printf("\n");
 
-	c_getch();
-	return w+h;
+    for (i = 2; i < h; i++) {
+        c_gotoxy(1, i);
+        printf("|"); 
+        for (j = 2; j < w; j++) {
+            printf(" "); 
+        if (w > 1) {
+            printf("|"); 
+        }
+        printf("\n");
+    }
 
+    if (h > 1) {
+        c_gotoxy(1, h);
+        printf("+");
+        for (i = 2; i < w; i++) {
+            printf("=");
+        }
+        if (w > 1) {
+            printf("+");
+        }
+        printf("\n");
+    } else if (h == 1 && w > 1) {
+        c_gotoxy(w, h);
+        printf("+");
+    } else if (h == 1 && w == 1) {
+        c_gotoxy(w, h);
+        printf("+");
+    }
+
+
+    c_getch();
+    return w + h;
+}
 }
