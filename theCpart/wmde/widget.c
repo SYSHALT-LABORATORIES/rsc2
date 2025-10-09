d77d6642e317d3989c682b3d1bddd8e8b6424e3d
df42Karazal <176348199+df42Karazal@users.noreply.github.com>
2025-10-09 12:57:19 -0300

    just so i dont need to stash (from vm)
    Signed-off-by: df42Karazal <176348199+df42Karazal@users.noreply.github.com>

--- theCpart/wmde/widget.c
+++ theCpart/wmde/widget.c
@@ -4,7 +4,7 @@
 #include "include/conio.c"
 #include "include/de.h"
 
-int widget(void) { // to compile separate use int main(void), this is for /kernel.c
+int main(void) { // to compile separate use int main(void), this is for /kernel.c
     int h, w;
     int i, j;
     int key_code;
@@ -19,33 +19,25 @@ int widget(void) { // to compile separate use int main(void), this is for /kerne
     char screen_buffer[2000]={' '};
 
     for(EVER) {
-        //c_clrscr();
+       // c_clrscr();
         draw_window(w, h, posx, posy);
-	if (_kbhit()) {
+	if (c_kbhit()) {
 		key_code = c_getch();
 		if (key_code == KB_UP) {
-			w=0;
-			h=0;
 			posy = 1000;
-			w = w * 5;
-			h = h * 10;
+			w  = w * 5;
+			h  = h * 10;
 		} if (key_code == KB_DOWN) {
-			w=0;
-			h=0;
 			posy = -1000;
-			w = w * 5;
+		        w = w * 5;
 			h = h * 10;
 		} if (key_code == KB_LEFT) {
-			w=0;
-			h=0;
 		       posx = -1000;
 	       	       w = w * 3;
 		       h = (h * 3) + w;	
 		} if (key_code == KB_RIGHT) {
-			w=0;
-			h=0;
 		        posx = 1000;
-			w = w * 5;
+		        w = w * 5;
 			h = h * 10;
 		}   	   
 	}
